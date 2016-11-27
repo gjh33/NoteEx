f95baed862b5ac236e5dd617041e8fb06a9c9f4b
Luke Jones <manhappylife@yahoo.ca>
2016-11-27 15:15:35 -0500

    Fix user save bug
    current_user does a query everytime
    didn't save
    help me

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -2,9 +2,11 @@ class Api::NotesController < ApiController
   def upload
     teaching = Teaching.find(params[:teaching_id])
     note = Note.new(attachment: params[:attachment], teaching: teaching)
+    debugger
     if note.save
-      current_user.points += 5
-      current_user.save!
+      user = current_user
+      user.points += 5
+      user.save!
       render_success
     else
       render_error("Upload Failed", note.errors.full_messages.first)
@@ -14,8 +16,9 @@ class Api::NotesController < ApiController
   def download
     note = Note.find(params[:id])
     if current_user.points > 0
-      current_user.points -= 1
-      current_user.save!
+      user = current_user
+      user.points -= 1
+      user.save!
       render_success(data: { url: note.attachment_url })
     else
       render_error("Not Enough Points", "The user does not have enough points for this download")
